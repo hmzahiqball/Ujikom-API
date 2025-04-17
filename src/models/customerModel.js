14f34328ad460d476ce675fe14c46cf5b5961c83
Putra28 <suyapratama2217@gmail.com>
2025-04-17 11:35:27 +0700

    updated deleteCustomer

--- src/models/customerModel.js
+++ src/models/customerModel.js
@@ -27,7 +27,7 @@ class Customer {
   }
 
   static async deleteCustomer(p_idCustomers) {
-    await db.query("DELETE FROM tb_customers WHERE id_customers = ?", [p_idCustomers]);
+    await db.query("UPDATE tb_customers SET status_customers = 'non-aktif WHERE id_customers = ?", [p_idCustomers]);
   }
 }
 
